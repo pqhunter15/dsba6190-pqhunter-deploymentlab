1c340126b6c834017c547c6ced69e6368f7de213
Patrick Hunter <pqhunter15@outlook.com>
2025-11-12 19:58:49 -0500

    setup terraform

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "patrickhunter"
+  default     = "pqh"
 }
 
 variable "class_name" {
