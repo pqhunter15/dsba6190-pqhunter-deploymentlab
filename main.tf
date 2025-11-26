a6b633b6ff974820b53ab4da9c199107c68872cf
Patrick Hunter <pqhunter15@outlook.com>
2025-11-26 14:40:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,41 +7,30 @@ locals {
   }
 }
 
-// Existing Resources
-
-/// Subscription ID
-
-# data "azurerm_subscription" "current" {
-# }
-
 // Random Suffix Generator
-
 resource "random_integer" "deployment_id_suffix" {
   min = 100
   max = 999
 }
 
 // Resource Group
-
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   location = var.location
-  is_hns_enabled = true
 
   tags = local.tags
 }
 
-
 // Storage Account
-
 resource "azurerm_storage_account" "storage" {
-  name                     = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind   = "StorageV2"
-  is_hns_enabled = true
+  account_kind             = "StorageV2"
+  is_hns_enabled           = true
 
   tags = local.tags
 
@@ -52,13 +41,12 @@ resource "azurerm_storage_account" "storage" {
   }
 }
 
-
-
 // Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   address_space = ["10.0.0.0/16"]
 
   tags = local.tags
@@ -69,7 +57,9 @@ resource "azurerm_subnet" "subnet" {
   name                 = "snet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
+
   address_prefixes = ["10.0.1.0/24"]
+
   service_endpoints = [
     "Microsoft.Storage",
     "Microsoft.Sql"
@@ -78,37 +68,34 @@ resource "azurerm_subnet" "subnet" {
 
 // SQL Server
 resource "azurerm_mssql_server" "sql" {
-  name                         = "sql-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
+  name                = "sql-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  version             = "12.0"
+
   administrator_login          = var.sql_admin_login
   administrator_login_password = var.sql_admin_password
+
   minimum_tls_version           = "1.2"
-  public_network_access_enabled = false
+  public_network_access_enabled = true   // needed for VNet rule to work
 
   tags = local.tags
-
-  resource "azurerm_mssql_virtual_network_rule" "sql_vnet_rule" {
-  name      = "sql-vnet-rule"
-  server_id = azurerm_mssql_server.sql.id
-  subnet_id = azurerm_subnet.subnet.id
-}
 }
 
-// SQL Database
+// SQL Database (Standard tier -> S0)
 resource "azurerm_mssql_database" "db" {
   name      = "sqldb-${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}"
   server_id = azurerm_mssql_server.sql.id
-  sku_name    = "Standard"
+
+  sku_name    = "S0"   // Standard family
   max_size_gb = 2
-  public_network_access_enabled = false
 
   tags = local.tags
+}
 
+// SQL VNet Rule (applies to the server, covers all DBs)
 resource "azurerm_mssql_virtual_network_rule" "sql_vnet_rule" {
   name      = "sql-vnet-rule"
   server_id = azurerm_mssql_server.sql.id
   subnet_id = azurerm_subnet.subnet.id
-}
 }
